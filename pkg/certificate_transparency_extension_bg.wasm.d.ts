8fe0f685816336ccedde1fd98e40f8753d2e4c13
NTV2k5 <nguyentanviet18012005@gmail.com>
2025-02-27 23:02:09 +0700

    Include pkg directory

---
+++ pkg/certificate_transparency_extension_bg.wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const run: () => void;
+export const check_certificate: () => void;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_start: () => void;
